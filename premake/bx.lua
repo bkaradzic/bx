a36d56908d08e0460ca22357c0f75525c6f2484a
Branimir Karadzic <bk@f-m.fm>
2013-10-17 20:13:11 -0700

    OSX: Fixed unit test build.

--- premake/bx.lua
+++ premake/bx.lua
@@ -7,6 +7,16 @@ project "bx"
 	uuid "4db0b09e-d6df-11e1-a0ec-65ccdd6a022f"
 	kind "StaticLib"
 
+	configuration { "osx or ios" }
+		-- OSX ar doesn't like creating archive without object files
+		-- here is object file...
+		prebuildcommands {
+			"@echo \"void dummy() {}\" > /tmp/dummy.cpp",
+		}
+		files {
+			"/tmp/dummy.cpp",
+		}
+
 	files {
 		"../include/**.h",
 	}
