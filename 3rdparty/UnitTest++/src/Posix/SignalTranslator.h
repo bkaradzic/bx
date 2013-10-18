974d2d707808a1458f7595047520bbd19d5ec700
Branimir Karadzic <bk@f-m.fm>
2013-10-17 20:12:35 -0700

    Fixed Clang warning.

--- 3rdparty/UnitTest++/src/Posix/SignalTranslator.h
+++ 3rdparty/UnitTest++/src/Posix/SignalTranslator.h
@@ -22,8 +22,8 @@ private:
     struct sigaction m_old_SIGTRAP_action;
     struct sigaction m_old_SIGSEGV_action;
     struct sigaction m_old_SIGBUS_action;
-    struct sigaction m_old_SIGABRT_action;
-    struct sigaction m_old_SIGALRM_action;
+//    struct sigaction m_old_SIGABRT_action;
+//    struct sigaction m_old_SIGALRM_action;
 };
 
 #if !defined (__GNUC__)
