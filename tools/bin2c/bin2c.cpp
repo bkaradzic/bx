7b13d15cfa4ee0d82360e72d3f614492ace6592e
Branimir Karadžić <branimirkaradzic@gmail.com>
2015-01-01 15:02:37 -0800

    Updated copyright year.

--- tools/bin2c/bin2c.cpp
+++ tools/bin2c/bin2c.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2014 Branimir Karadzic. All rights reserved.
+ * Copyright 2011-2015 Branimir Karadzic. All rights reserved.
  * License: http://www.opensource.org/licenses/BSD-2-Clause
  */
 
@@ -95,7 +95,7 @@ void help(const char* _error = NULL)
 
 	fprintf(stderr
 		, "bin2c, binary to C\n"
-		  "Copyright 2011-2014 Branimir Karadzic. All rights reserved.\n"
+		  "Copyright 2011-2015 Branimir Karadzic. All rights reserved.\n"
 		  "License: http://www.opensource.org/licenses/BSD-2-Clause\n\n"
 		);
 
