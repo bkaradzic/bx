ef90d6325ad0f64b56ff040f1c85a96fa9681f09
Branimir Karadžić <branimirkaradzic@gmail.com>
2017-02-08 18:20:40 -0800

    Cleanup.

--- include/bx/platform.h
+++ include/bx/platform.h
@@ -72,7 +72,7 @@
 #	elif defined(__MINGW32__) || defined(__MINGW64__)
 #		undef  BX_CRT_MINGW
 #		define BX_CRT_MINGW 1
-#	elif defined(__apple_build_version__) || defined(__ANDROID__)
+#	elif defined(__apple_build_version__) || defined(__ANDROID__) || defined(__ORBIS__)
 #		undef  BX_CRT_LIBCXX
 #		define BX_CRT_LIBCXX 1
 #	endif //
