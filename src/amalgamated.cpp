6957cdd9e219e60a3323a9cb9fb5555f67b64fc5
Branimir Karadžić <branimirkaradzic@gmail.com>
2017-02-15 23:56:40 -0800

    Fixed amalgamated build.

--- src/amalgamated.cpp
+++ src/amalgamated.cpp
@@ -3,15 +3,16 @@
  *  License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
  */
 
+#include "bx.cpp"
 #include "commandline.cpp"
-#include "crt.cpp"
 #include "crtimpl.cpp"
+#include "crtnone.cpp"
 #include "debug.cpp"
 #include "dtoa.cpp"
 #include "fpumath.cpp"
 #include "mutex.cpp"
 #include "os.cpp"
-#include "sem.cpp"
+#include "semaphore.cpp"
 #include "sort.cpp"
 #include "string.cpp"
 #include "thread.cpp"
