bd2ea1cbde76afb4d5d9f39329982412783816d5
Zachary Carter <carterza@gmail.com>
2017-03-19 14:17:35 -0400

    Set AR to NDK ARM. Fixes - https://github.com/bkaradzic/bgfx/issues/1022 (#144)
    * Since system AR seems to be failing...
    
    * Forgot end in toolchain.lua.
    
    * Leveraging ANDROID_NDK_ARM
    
    * Updating warning message when ANDROID_NDK_ARM isn't set.

--- scripts/toolchain.lua
+++ scripts/toolchain.lua
@@ -205,11 +205,13 @@ function toolchain(_buildDir, _libDir)
 			if not os.getenv("ANDROID_NDK_ARM")
 			or not os.getenv("ANDROID_NDK_CLANG")
 			or not os.getenv("ANDROID_NDK_ROOT") then
-				print("Set ANDROID_NDK_CLANG and ANDROID_NDK_ROOT envrionment variables.")
+				print("Set ANDROID_NDK_CLANG, ANDROID_NDK_ARM, and ANDROID_NDK_ROOT envrionment variables.")
 			end
 
 			premake.gcc.cc   = "$(ANDROID_NDK_CLANG)/bin/clang"
 			premake.gcc.cxx  = "$(ANDROID_NDK_CLANG)/bin/clang++"
+			premake.gcc.ar = "$(ANDROID_NDK_ARM)/bin/arm-linux-androideabi-ar"
+
 			premake.gcc.llvm = true
 			location (path.join(_buildDir, "projects", _ACTION .. "-android-arm"))
 
