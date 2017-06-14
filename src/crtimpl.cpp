658fd5376fd0a21c5e94174bf6f50f458edaaf92
Branimir Karadžić <branimirkaradzic@gmail.com>
2017-06-13 22:05:49 -0700

    Cleanup.

--- src/crtimpl.cpp
+++ src/crtimpl.cpp
@@ -8,6 +8,22 @@
 #include <stdio.h>
 #include <malloc.h>
 
+#ifndef BX_CONFIG_CRT_FILE_READER_WRITER
+#	define BX_CONFIG_CRT_FILE_READER_WRITER !(0 \
+			|| BX_CRT_NONE                      \
+			)
+#endif // BX_CONFIG_CRT_FILE_READER_WRITER
+
+#ifndef BX_CONFIG_CRT_PROCESS
+#	define BX_CONFIG_CRT_PROCESS !(0  \
+			|| BX_CRT_NONE            \
+			|| BX_PLATFORM_EMSCRIPTEN \
+			|| BX_PLATFORM_PS4        \
+			|| BX_PLATFORM_WINRT      \
+			|| BX_PLATFORM_XBOXONE    \
+			)
+#endif // BX_CONFIG_CRT_PROCESS
+
 namespace bx
 {
 	DefaultAllocator::DefaultAllocator()
