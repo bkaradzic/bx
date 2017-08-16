277151ee98dc70a86865a1ca17426796cc1c5a08
Krzysztof Major <krzysztof.major@gmail.com>
2017-08-16 06:27:08 +0100

    remove ITERATOR_DEBUGGING

--- scripts/toolchain.lua
+++ scripts/toolchain.lua
@@ -552,8 +552,7 @@ function toolchain(_buildDir, _libDir)
 		defines {
 			"WIN32",
 			"_WIN32",
-			"_HAS_EXCEPTIONS=0",
-			"_HAS_ITERATOR_DEBUGGING=0",
+			"_HAS_EXCEPTIONS=0",			
 			"_SCL_SECURE=0",
 			"_SECURE_SCL=0",
 			"_SCL_SECURE_NO_WARNINGS",
