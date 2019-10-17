1e103a68e78940f6dca3589853ea975d04bf128c
Thomas Klausner <tk@giga.or.at>
2019-10-17 15:59:37 +0200

    Improve BSD support, tested on NetBSD.

--- src/mutex.cpp
+++ src/mutex.cpp
@@ -17,6 +17,7 @@
 	|| BX_PLATFORM_IOS     \
 	|| BX_PLATFORM_OSX     \
 	|| BX_PLATFORM_PS4     \
+	|| BX_PLATFORM_BSD     \
 	|| BX_PLATFORM_RPI
 #	include <pthread.h>
 #elif  BX_PLATFORM_WINDOWS \
