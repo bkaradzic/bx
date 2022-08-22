bkaradzic/bx

commit 7929f098f559af2ea7cc440fef2a7eae51d9a6fc

Fixed bx::sign function to return tri-state result.

tests/math_test.cpp
@@ -210,6 +210,13 @@ TEST_CASE("libm", "")
 	REQUIRE(bx::isEqual(bx::atan2(0.0f, 0.0f), ::atan2f(0.0f, 0.0f), 0.00001f) );
 }
 
+TEST_CASE("sign", "")
+{
+	REQUIRE(-1 == bx::sign(-0.1389f) );
+	REQUIRE( 0 == bx::sign( 0.0000f) );
+	REQUIRE( 1 == bx::sign( 0.1389f) );
+}
+
 TEST_CASE("ToBits", "")
 {
 	REQUIRE(UINT32_C(0x12345678)         == bx::floatToBits( bx::bitsToFloat( UINT32_C(0x12345678) ) ) );

include/bx/inline/math.inl
@@ -135,7 +135,7 @@ namespace bx
 
 	inline BX_CONSTEXPR_FUNC float sign(float _a)
 	{
-		return _a < 0.0f ? -1.0f : 1.0f;
+		return (0.0f < _a) - (0.0f > _a);
 	}
 
 	inline BX_CONSTEXPR_FUNC float abs(float _a)

include/bx/math.h
@@ -205,7 +205,11 @@ namespace bx
 	///
 	BX_CONSTEXPR_FUNC float invLerp(float _a, float _b, float _value);
 
-	/// Returns the sign of _a.
+	/// Extracts the sign of value `_a`.
+	///
+	/// @param[in] _a Value.
+	///
+	/// @returns -1 if `_a` less than zero, 0 if `_a` is equal to 0, or +1 if `_a` is greater than zero.
 	///
 	BX_CONSTEXPR_FUNC float sign(float _a);
 

commit 7d784e74ff3ff9e2673f48f7e258f3de0bea001b

Fixed atan2. Issue #282. Caused by 7929f098f559af2ea7cc440fef2a7eae51d9a6fc.

src/math.cpp
@@ -125,7 +125,7 @@ namespace bx
 		const float tmp5   = tmp4 * mxy;
 		const float tmp6   = ay > ax   ? kPiHalf - tmp5 : tmp5;
 		const float tmp7   = _x < 0.0f ? kPi     - tmp6 : tmp6;
-		const float result = sign(_y)*tmp7;
+		const float result = (_y < 0.0f ? -1.0f : 1.0f)*tmp7;
 
 		return result;
 	}

tests/math_test.cpp
@@ -63,6 +63,7 @@ TEST_CASE("log2", "")
 TEST_CASE("libm", "")
 {
 	bx::WriterI* writer = bx::getNullOut();
+	bx::Error err;
 
 	REQUIRE(1389.0f == bx::abs(-1389.0f) );
 	REQUIRE(1389.0f == bx::abs( 1389.0f) );
@@ -80,8 +81,6 @@ TEST_CASE("libm", "")
 	REQUIRE(-13.0f == bx::trunc(-13.89f) );
 	REQUIRE(bx::isEqual( 0.89f, bx::fract( 13.89f), 0.000001f) );
 	REQUIRE(bx::isEqual(-0.89f, bx::fract(-13.89f), 0.000001f) );
-
-	bx::Error err;
 
 	for (int32_t yy = -10; yy < 10; ++yy)
 	{
@@ -196,6 +195,16 @@ TEST_CASE("libm", "")
 		REQUIRE(err.isOk() );
 		REQUIRE(bx::isEqual(bx::atan(xx), ::atanf(xx), 0.00001f) );
 	}
+}
+
+TEST_CASE("atan2", "")
+{
+	bx::WriterI* writer = bx::getNullOut();
+	bx::Error err;
+
+	REQUIRE(bx::isEqual(bx::atan2(0.0f,  0.0f), ::atan2f(0.0f,  0.0f), 0.00001f) );
+	REQUIRE(bx::isEqual(bx::atan2(0.0f,  1.0f), ::atan2f(0.0f,  1.0f), 0.00001f) );
+	REQUIRE(bx::isEqual(bx::atan2(0.0f, -1.0f), ::atan2f(0.0f, -1.0f), 0.00001f) );
 
 	for (float yy = -100.0f; yy < 100.0f; yy += 0.1f)
 	{
@@ -206,8 +215,6 @@ TEST_CASE("libm", "")
 			REQUIRE(bx::isEqual(bx::atan2(yy, xx), ::atan2f(yy, xx), 0.00001f) );
 		}
 	}
-
-	REQUIRE(bx::isEqual(bx::atan2(0.0f, 0.0f), ::atan2f(0.0f, 0.0f), 0.00001f) );
 }
 
 TEST_CASE("sign", "")