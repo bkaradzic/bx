9e7fab8f5402dad68551fe05c439f7277ec8faa3
Trung LE <trung.le@ruby-journal.com>
2022-11-14 00:31:52 +1100

    Support PPC Linux

--- scripts/toolchain.lua
+++ scripts/toolchain.lua
@@ -78,6 +78,7 @@ function toolchain(_buildDir, _libDir)
 			{ "linux-clang-afl", "Linux (Clang + AFL fuzzer)" },
 			{ "linux-mips-gcc",  "Linux (MIPS, GCC compiler)" },
 			{ "linux-arm-gcc",   "Linux (ARM, GCC compiler)"  },
+			{ "linux-ppc-gcc",   "Linux (PPC, GCC compiler)"  },
 			{ "ios-arm",         "iOS - ARM"                  },
 			{ "ios-arm64",       "iOS - ARM64"                },
 			{ "ios-simulator",   "iOS - Simulator"            },
@@ -311,6 +312,9 @@ function toolchain(_buildDir, _libDir)
 
 		elseif "linux-arm-gcc" == _OPTIONS["gcc"] then
 			location (path.join(_buildDir, "projects", _ACTION .. "-linux-arm-gcc"))
+
+		elseif "linux-ppc-gcc" == _OPTIONS["gcc"] then
+ 			location (path.join(_buildDir, "projects", _ACTION .. "-linux-ppc-gcc"))
 
 		elseif "mingw-gcc" == _OPTIONS["gcc"] then
 			if not os.getenv("MINGW") then
@@ -863,6 +867,22 @@ function toolchain(_buildDir, _libDir)
 		linkoptions {
 			"-s MAX_WEBGL_VERSION=2",
 		}
+
+	configuration { "linux-ppc-gcc" }
+ 		targetdir (path.join(_buildDir, "linux32_ppc_gcc/bin"))
+ 		objdir (path.join(_buildDir, "linux32_ppc_gcc/obj"))
+ 		libdirs { path.join(_libDir, "lib/linux32_ppc_gcc") }
+ 		buildoptions {
+ 			"-Wunused-value",
+ 			"-Wundef",
+ 		}
+ 		links {
+ 			"rt",
+ 			"dl",
+ 		}
+ 		linkoptions {
+ 			"-Wl,--gc-sections",
+ 		}
 
 	configuration { "wasm2js" }
 		targetdir (path.join(_buildDir, "wasm2js/bin"))
