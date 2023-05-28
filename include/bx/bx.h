105769297b7034fefabde5510eae5db6ba703c95
Cory Perry <killogge@gmail.com>
2023-05-28 02:18:44 -0700

    Fix MSVC include alloca error
    On MSVC 2022 alloca.h doesn't exist.  alloca actually comes from malloc.h.  Since the platform header comes in later, just use the standard _WIN32 define here.

--- include/bx/bx.h
+++ include/bx/bx.h
@@ -6,7 +6,11 @@
 #ifndef BX_H_HEADER_GUARD
 #define BX_H_HEADER_GUARD
 
+#if defined(_WIN32)
+#include <malloc.h>	// alloca
+#else
 #include <alloca.h> // alloca
+#endif
 #include <stdarg.h> // va_list
 #include <stdint.h> // uint32_t
 #include <stdlib.h> // size_t
